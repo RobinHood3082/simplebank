09e14ffc74a777e83e06fe72670a50fedc2841f4
RobinHood3082 <rkhan3082@gmail.com>
2024-08-28 21:19:10 +0600

    add: transaction deadlock unit test

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -106,3 +106,52 @@ func TestTransferTx(t *testing.T) {
 	require.Equal(t, account1.Balance-n*amount, updatedAccount1.Balance)
 	require.Equal(t, account2.Balance+n*amount, updatedAccount2.Balance)
 }
+
+func TestTransferTxDeadlock(t *testing.T) {
+	store := NewStore(testDB)
+
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+
+	// run n concurrent transaction
+	n := int64(10)
+	amount := int64(10)
+
+	errors := make(chan error)
+
+	for i := range n {
+		fromAccountID := account1.ID
+		toAccountID := account2.ID
+
+		if i%2 == 1 {
+			fromAccountID = account2.ID
+			toAccountID = account1.ID
+		}
+
+		go func() {
+			_, err := store.TransferTx(context.Background(), TransferTxParams{
+				FromAccountID: fromAccountID,
+				ToAccountID:   toAccountID,
+				Amount:        amount,
+			})
+
+			errors <- err
+		}()
+	}
+
+	// check results
+	for range n {
+		err := <-errors
+		require.NoError(t, err)
+
+	}
+
+	updatedAccount1, err := store.GetAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+
+	updatedAccount2, err := store.GetAccount(context.Background(), account2.ID)
+	require.NoError(t, err)
+
+	require.Equal(t, account1.Balance, updatedAccount1.Balance)
+	require.Equal(t, account2.Balance, updatedAccount2.Balance)
+}
