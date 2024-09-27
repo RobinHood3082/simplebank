7a48b29afaf83a2cccd6e4fec314f8152c4f4647
RobinHood3082 <rkhan3082@gmail.com>
2024-09-27 20:43:49 +0600

    add: auth middleware

--- internal/app/middlewares.go
+++ internal/app/middlewares.go
@@ -1,8 +1,11 @@
 package app
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 // LogRequest logs all incoming requests
@@ -23,5 +26,48 @@ func (server *Server) RecoverPanic(next http.HandlerFunc) http.HandlerFunc {
 			}
 		}()
 		next.ServeHTTP(w, r)
+	})
+}
+
+const (
+	authorizationHeaderKey  = "authorization"
+	authorizationTypeBearer = "bearer"
+	AuthorizationPayloadKey = AutorizationPayloadKey("authorization_payload")
+)
+
+// AutorizationPayloadKey is the key for the authorization payload in the request context
+type AutorizationPayloadKey string
+
+// Authenticate checks if the request is authenticated
+func (server *Server) Authenticate(next http.HandlerFunc) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		server.logger.Info("Authenticating request")
+		authorizationHeader := r.Header.Get(authorizationHeaderKey)
+		if len(authorizationHeader) == 0 {
+			server.writeError(w, http.StatusUnauthorized, errors.New("authorization header is missing"))
+			return
+		}
+
+		fields := strings.Fields(authorizationHeader)
+		if len(fields) != 2 {
+			server.writeError(w, http.StatusUnauthorized, errors.New("authorization header is not in the format 'Bearer <token>'"))
+			return
+		}
+
+		authorizationType := strings.ToLower(fields[0])
+		if authorizationType != authorizationTypeBearer {
+			server.writeError(w, http.StatusUnauthorized, fmt.Errorf("unsupported authorization type %s", authorizationType))
+			return
+		}
+
+		accessToken := fields[1]
+		payload, err := server.tokenMaker.VerifyToken(accessToken)
+		if err != nil {
+			server.writeError(w, http.StatusUnauthorized, err)
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), AuthorizationPayloadKey, payload)
+		next.ServeHTTP(w, r.WithContext(ctx))
 	})
 }
