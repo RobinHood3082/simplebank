290f862911e2e777b3f225fd0be3f4bf8c416ac1
RobinHood3082 <rkhan3082@gmail.com>
2024-10-04 20:36:12 +0600

    add: login_user rpc

---
+++ internal/gapi/rpc_login_user.go
@@ -0,0 +1,69 @@
+package gapi
+
+import (
+	"context"
+
+	"github.com/RobinHood3082/simplebank/internal/pb"
+	"github.com/RobinHood3082/simplebank/internal/persistence"
+	"github.com/RobinHood3082/simplebank/util"
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgtype"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+func (server *Server) LoginUser(ctx context.Context, req *pb.LoginUserRequest) (*pb.LoginUserResponse, error) {
+	user, err := server.store.GetUser(ctx, req.GetUsername())
+	if err != nil {
+		if err == pgx.ErrNoRows {
+			return nil, status.Errorf(codes.NotFound, "user not found")
+		}
+		return nil, status.Errorf(codes.Internal, "failed to get user")
+	}
+
+	if err := util.CheckPassword(req.GetPassword(), user.HashedPassword); err != nil {
+		return nil, status.Errorf(codes.Unauthenticated, "invalid password")
+	}
+
+	accessToken, accessPayload, err := server.tokenMaker.CreateToken(
+		user.Username,
+		server.config.AccessTokenDuration,
+	)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to create access token")
+	}
+
+	refreshToken, refreshPayload, err := server.tokenMaker.CreateToken(
+		user.Username,
+		server.config.RefreshTokenDuration,
+	)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to create refresh token")
+	}
+
+	session, err := server.store.CreateSession(ctx, persistence.CreateSessionParams{
+		ID:           pgtype.UUID{Bytes: refreshPayload.ID, Valid: true},
+		Username:     user.Username,
+		RefreshToken: refreshToken,
+		UserAgent:    "",
+		ClientIp:     "",
+		IsBlocked:    false,
+		ExpiresAt:    pgtype.Timestamptz{Time: refreshPayload.ExpiredAt, Valid: true},
+	})
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to create session")
+	}
+
+	rsp := &pb.LoginUserResponse{
+		SessionId:             uuid.UUID(session.ID.Bytes).String(),
+		AccessToken:           accessToken,
+		AccessTokenExpiresAt:  timestamppb.New(accessPayload.ExpiredAt),
+		RefreshToken:          refreshToken,
+		RefreshTokenExpiresAt: timestamppb.New(refreshPayload.ExpiredAt),
+		User:                  convertUser(user),
+	}
+
+	return rsp, nil
+}
