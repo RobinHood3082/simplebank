3b78a5115b95ee17e82477f2439aa7e2f84b0999
RobinHood3082 <rkhan3082@gmail.com>
2024-10-05 10:50:04 +0600

    add: extract info from gRPC metadata

--- internal/gapi/rpc_login_user.go
+++ internal/gapi/rpc_login_user.go
@@ -43,12 +43,13 @@ func (server *Server) LoginUser(ctx context.Context, req *pb.LoginUserRequest) (
 		return nil, status.Errorf(codes.Internal, "failed to create refresh token")
 	}
 
+	mtdt := server.extractMetadata(ctx)
 	session, err := server.store.CreateSession(ctx, persistence.CreateSessionParams{
 		ID:           pgtype.UUID{Bytes: refreshPayload.ID, Valid: true},
 		Username:     user.Username,
 		RefreshToken: refreshToken,
-		UserAgent:    "",
-		ClientIp:     "",
+		UserAgent:    mtdt.UserAgent,
+		ClientIp:     mtdt.ClientIp,
 		IsBlocked:    false,
 		ExpiresAt:    pgtype.Timestamptz{Time: refreshPayload.ExpiredAt, Valid: true},
 	})
