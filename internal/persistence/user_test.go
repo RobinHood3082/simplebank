b7ad76d211d6cd7d9541145961487cf09e91c4a1
RobinHood3082 <rkhan3082@gmail.com>
2024-10-06 17:29:46 +0600

    feat: update user

--- internal/persistence/user_test.go
+++ internal/persistence/user_test.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/RobinHood3082/simplebank/util"
+	"github.com/jackc/pgx/v5/pgtype"
 	"github.com/stretchr/testify/require"
 )
 
@@ -52,4 +53,128 @@ func TestGetUser(t *testing.T) {
 	require.Equal(t, user1.FullName, user2.FullName)
 	require.WithinDuration(t, user1.CreatedAt.Time, user2.CreatedAt.Time, time.Second)
 	require.WithinDuration(t, user1.PasswordChangedAt.Time, user2.PasswordChangedAt.Time, time.Second)
+}
+
+func TestUpdateUserOnlyFullName(t *testing.T) {
+	oldUser := createRandomUser(t)
+
+	newFullName := util.RandomOwner()
+	updatedUser, err := testQueries.UpdateUser(context.Background(),
+		UpdateUserParams{
+			Username: oldUser.Username,
+			FullName: pgtype.Text{
+				String: newFullName,
+				Valid:  true,
+			},
+		},
+	)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, updatedUser)
+
+	require.Equal(t, oldUser.Username, updatedUser.Username)
+	require.Equal(t, oldUser.HashedPassword, updatedUser.HashedPassword)
+	require.Equal(t, oldUser.Email, updatedUser.Email)
+	require.Equal(t, newFullName, updatedUser.FullName)
+	require.WithinDuration(t, oldUser.CreatedAt.Time, updatedUser.CreatedAt.Time, time.Second)
+	require.WithinDuration(t, oldUser.PasswordChangedAt.Time, updatedUser.PasswordChangedAt.Time, time.Second)
+
+	require.NotEqual(t, oldUser.FullName, updatedUser.FullName)
+}
+
+func TestUpdateUserOnlyEmail(t *testing.T) {
+	oldUser := createRandomUser(t)
+
+	newEmail := util.RandomEmail()
+	updatedUser, err := testQueries.UpdateUser(context.Background(),
+		UpdateUserParams{
+			Username: oldUser.Username,
+			Email: pgtype.Text{
+				String: newEmail,
+				Valid:  true,
+			},
+		},
+	)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, updatedUser)
+
+	require.Equal(t, oldUser.Username, updatedUser.Username)
+	require.Equal(t, oldUser.HashedPassword, updatedUser.HashedPassword)
+	require.Equal(t, oldUser.FullName, updatedUser.FullName)
+	require.Equal(t, newEmail, updatedUser.Email)
+	require.WithinDuration(t, oldUser.CreatedAt.Time, updatedUser.CreatedAt.Time, time.Second)
+	require.WithinDuration(t, oldUser.PasswordChangedAt.Time, updatedUser.PasswordChangedAt.Time, time.Second)
+
+	require.NotEqual(t, oldUser.Email, updatedUser.Email)
+}
+
+func TestUpdateUserOnlyPassword(t *testing.T) {
+	oldUser := createRandomUser(t)
+
+	newPassword := util.RandomString(6)
+	newHashedPassword, err := util.HashPassword(newPassword)
+	require.NoError(t, err)
+
+	updatedUser, err := testQueries.UpdateUser(context.Background(),
+		UpdateUserParams{
+			Username: oldUser.Username,
+			HashedPassword: pgtype.Text{
+				String: newHashedPassword,
+				Valid:  true,
+			},
+		},
+	)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, updatedUser)
+
+	require.Equal(t, oldUser.Username, updatedUser.Username)
+	require.Equal(t, updatedUser.HashedPassword, newHashedPassword)
+	require.Equal(t, oldUser.FullName, updatedUser.FullName)
+	require.Equal(t, oldUser.Email, updatedUser.Email)
+	require.WithinDuration(t, oldUser.CreatedAt.Time, updatedUser.CreatedAt.Time, time.Second)
+
+	require.NotEqual(t, oldUser.HashedPassword, updatedUser.HashedPassword)
+}
+
+func TestUpdateUserAllFields(t *testing.T) {
+	oldUser := createRandomUser(t)
+
+	newFullName := util.RandomOwner()
+	newEmail := util.RandomEmail()
+	newPassword := util.RandomString(6)
+	newHashedPassword, err := util.HashPassword(newPassword)
+	require.NoError(t, err)
+
+	updatedUser, err := testQueries.UpdateUser(context.Background(),
+		UpdateUserParams{
+			Username: oldUser.Username,
+			FullName: pgtype.Text{
+				String: newFullName,
+				Valid:  true,
+			},
+			Email: pgtype.Text{
+				String: newEmail,
+				Valid:  true,
+			},
+			HashedPassword: pgtype.Text{
+				String: newHashedPassword,
+				Valid:  true,
+			},
+		},
+	)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, updatedUser)
+
+	require.Equal(t, oldUser.Username, updatedUser.Username)
+	require.Equal(t, newHashedPassword, updatedUser.HashedPassword)
+	require.Equal(t, newFullName, updatedUser.FullName)
+	require.Equal(t, newEmail, updatedUser.Email)
+	require.WithinDuration(t, oldUser.CreatedAt.Time, updatedUser.CreatedAt.Time, time.Second)
+
+	require.NotEqual(t, oldUser.FullName, updatedUser.FullName)
+	require.NotEqual(t, oldUser.Email, updatedUser.Email)
+	require.NotEqual(t, oldUser.HashedPassword, updatedUser.HashedPassword)
 }
