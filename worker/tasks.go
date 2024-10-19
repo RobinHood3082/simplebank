c014fbd01d729c3ada2707ac6767c983e14753e3
RobinHood3082 <rkhan3082@gmail.com>
2024-10-19 13:28:37 +0600

    refactor: move task relevant code to tasks.go

--- worker/tasks.go
+++ worker/tasks.go
@@ -3,6 +3,7 @@ package worker
 const (
 	TaskSendAccountCreatedEmail = "task:send_account_created_email"
 	TaskSendVerifyEmail         = "task:send_verify_email"
+	TaskSendBalanceAddedEmail   = "task:send_balance_added_email"
 )
 
 type PayloadSendAccountCreatedEmail struct {
@@ -14,4 +15,12 @@ type PayloadSendAccountCreatedEmail struct {
 
 type PayloadSendVerifyEmail struct {
 	Username string `json:"username"`
+}
+
+type PayloadSendBalanceAddedEmail struct {
+	Username     string `json:"username"`
+	AccountID    string `json:"account_id"`
+	AddedBalance int64  `json:"added_balance"`
+	Currency     string `json:"currency"`
+	NewBalance   int64  `json:"new_balance"`
 }
